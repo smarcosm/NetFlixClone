6490746994401dd5a41ae0e292facc4a933650be
Sebastião Marcos <sebastiaomarcos49@gmail.com>
2022-01-18 05:38:13 -0300

    components_MovieRow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //pegando a lista de filmes
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
   return (
-    <div>
-      Olá Mundo!
+    <div className="page">
+      <section className="lists">
+       {movieList.map((item, key) =>(
+         <MovieRow key={key}/>
+         
+       ))} 
+
+      </section>
+
     </div>
   )
 }
